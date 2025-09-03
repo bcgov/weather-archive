7393167708171351545365e13769864da7ad2eac
sgillbc <sukhbir.gill@gov.bc.ca>
2025-09-03 13:55:27 -0700

    insert header style before head closing

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -5,7 +5,7 @@ HEADER_COLOR=${HEADER_COLOR:-#448a38}
 
 # Change NAV color based on environment
 if [ "$ENVIRONMENT" != "production" ]; then
-    sed -i "/<head>/a <style>.bg-primary-nav { background-color: $HEADER_COLOR !important; }</style>" /usr/share/nginx/html/index.html
+    sed -i "/<\/head>/i <style>.bg-primary-nav { background-color: $HEADER_COLOR !important; }</style>" /usr/share/nginx/html/index.html
 fi
 
 # Start nginx
