5c5f3a9a63a4f72246408bafff13f847f57de4da
sgillbc <sukhbir.gill@gov.bc.ca>
2025-09-03 13:38:58 -0700

    add header colors based on environment

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ENVIRONMENT=${ENVIRONMENT:-development}
+HEADER_COLOR=${HEADER_COLOR:-#448a38}
+
+# Change NAV color based on environment
+if [ "$ENVIRONMENT" != "production" ]; then
+    sed -i "/<head>/a <style>.bg-primary-nav { background-color: $HEADER_COLOR !important; }</style>" /usr/share/nginx/html/index.html
+fi
+
+# Start nginx
+exec "$@"
